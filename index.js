2c021a8da1724b739729c21108935a617691e048
Xola <xolabangaza@gmail.com>
2024-02-22 13:08:06 +0200

    m

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-  var currentIndex = 0;
-  var slides = $('.slider img');
-  var bubblesContainer = $('.bubbles');
-
-  // Create bubbles
-  slides.each(function(index) {
+    var currentIndex = 0;
+    var slides = $('.slider .slide');
+    var bubblesContainer = $('.bubbles');
+  
+    // Create bubbles
+    slides.each(function(index) {
       var bubble = $('<div class="bubble"></div>');
       bubble.click(function() {
-          currentIndex = index;
-          updateSlider();
+        currentIndex = index;
+        updateSlider();
       });
       bubblesContainer.append(bubble);
-  });
-
-  function updateSlider() {
+    });
+  
+    function updateSlider() {
       // Hide all slides
       slides.hide();
       // Show current slide
@@ -22,14 +22,31 @@ $(document).ready(function() {
       bubblesContainer.find('.bubble').removeClass('active');
       // Add active class to current bubble
       bubblesContainer.find('.bubble').eq(currentIndex).addClass('active');
-  }
-
-  // Next arrow click event
-  $('.arrow.next').click(function() {
+    }
+  
+    // Next arrow click event
+    $('.arrow.next').click(function() {
       currentIndex = (currentIndex + 1) % slides.length;
       updateSlider();
+    });
+  
+    // Automatic sliding
+    function autoSlide() {
+      currentIndex = (currentIndex + 1) % slides.length;
+      updateSlider();
+    }
+  
+    // Set interval for automatic sliding
+    var intervalId = setInterval(autoSlide, 3000);
+  
+    // Pause automatic sliding on hover
+    $('.slider').hover(function() {
+      clearInterval(intervalId);
+    }, function() {
+      intervalId = setInterval(autoSlide, 3000);
+    });
+  
+    // Initial slide display
+    updateSlider();
   });
-
-  // Initial slide display
-  updateSlider();
-});
+  
